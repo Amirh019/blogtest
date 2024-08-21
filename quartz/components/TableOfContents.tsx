06aa7443d8081dbf74f82914088ec7c7324c9954
amirh019 <ayyoob.mohaghegh@gmail.com>
2024-08-21 17:19:50 +0330

    Quartz sync: Aug 21, 2024, 5:19 PM

--- quartz/components/TableOfContents.tsx
+++ quartz/components/TableOfContents.tsx
@@ -33,7 +33,7 @@ const TableOfContents: QuartzComponent = ({
         aria-controls="toc-content"
         aria-expanded={!fileData.collapseToc}
       >
-        <h3>{i18n(cfg.locale).components.tableOfContents.title}</h3>
+      
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="24"
@@ -46,6 +46,7 @@ const TableOfContents: QuartzComponent = ({
           stroke-linejoin="round"
           class="fold"
         >
+        <h3>{i18n(cfg.locale).components.tableOfContents.title}</h3>
           <polyline points="6 9 12 15 18 9"></polyline>
         </svg>
       </button>
